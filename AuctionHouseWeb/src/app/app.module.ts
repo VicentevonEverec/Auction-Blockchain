d3e02cb6a0848ba87cf4e03d7f813343823d5765
VicentevonEverec <vjmr1@alu.ua.es>
2023-07-22 19:16:28 +0200

    Initial moqup, sidenav functional

--- AuctionHouseWeb/src/app/app.module.ts
+++ AuctionHouseWeb/src/app/app.module.ts
@@ -1,24 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LogoComponent } from './logo/logo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {NgIf} from '@angular/common';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     HeaderComponent,
     FooterComponent,
-    LogoComponent
+    LogoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule.forRoot(),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    NgIf
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AuctionHouseWeb/src/app/home/home.component.ts
+++ AuctionHouseWeb/src/app/home/home.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss', './home-sidebar.component.scss']
 })
 export class HomeComponent {
+  carouselImages = [
+    { src: 'https://picsum.photos/id/7/600/400', alt: 'Imagen 1' },
+    { src: 'https://picsum.photos/id/12/600/400', alt: 'Imagen 2' },
+    { src: 'https://picsum.photos/id/24/600/400', alt: 'Imagen 3' },
+    // Agrega más imágenes al carrusel según tus necesidades
+  ];
+
+  showFiller = false;
 
 }
